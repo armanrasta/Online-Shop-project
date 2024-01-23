8830f2d554f2ae506251e21cbc756903d603c03f
armanrasta <62389951+armanrasta@users.noreply.github.com>
2024-01-23 13:22:29 +0330

    first step

---
+++ OnlineShopProject/Costumers/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+# Create your models here.
+
+class Costumer(models.Model):
+    
+    first_name = models.CharField(max_length=50)
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True, max_length = 11)
+    email = models.EmailField(null=False, blank=False, unique=True, max_length=254)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Address(models.Model):
+    
+    state = models.CharField()
+    city = models.CharField()
