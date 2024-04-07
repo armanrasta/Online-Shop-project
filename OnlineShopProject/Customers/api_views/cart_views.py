7727e7ff7e3c76a6aa29c5ab9e538749a1475156
armanrasta <62389951+armanrasta@users.noreply.github.com>
2024-04-07 18:47:48 +0330

    cart views update (pre improve)

--- OnlineShopProject/Customers/api_views/cart_views.py
+++ OnlineShopProject/Customers/api_views/cart_views.py
@@ -1,11 +1,13 @@
 from rest_framework.decorators import api_view, permission_classes,authentication_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework_simplejwt.authentication import JWTTokenUserAuthentication
 from ..models import Cart, CartItem, Product
 from ..serializers import CartItemSerializer, CartSerializer
 
+#shop
 @api_view(['POST'])
 @authentication_classes([JWTTokenUserAuthentication])
 @permission_classes([IsAuthenticated])
@@ -22,42 +24,49 @@ def add_to_cart(request):
             cart_item.quantity += int(quantity)
         cart_item.save()
 
-        return Response({'success': 'Item added to cart'}, status=status.HTTP_201_CREATED)
-    except Product.DoesNotExist:
-        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
-
-@api_view(['GET'])
-@authentication_classes([JWTTokenUserAuthentication])
-@permission_classes([IsAuthenticated])
-def show_cart(request):
-    user = request.user
-    try:
-        cart = Cart.objects.get(customer=user)
+        cart.refresh_from_db()
         serializer = CartSerializer(cart)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    except Cart.DoesNotExist:
-        return Response({'error': 'Cart not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {'success': 'Item added to cart', 'cart': serializer.data}, status=status.HTTP_201_CREATED)
+    except Product.DoesNotExist:
+        return Response(
+            {'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
     
-@api_view(['POST'])
+#cart
 @authentication_classes([JWTTokenUserAuthentication])
 @permission_classes([IsAuthenticated])
-def remove_from_cart(request):
-    user = request.user
-    product_id = request.data.get('product_id')
-    remove_entire_item = request.data.get('remove_entire_item', False)
+class CartView(APIView):
+    def get(self, request): #show cart
+        user = request.user
+        try:
+            cart = Cart.objects.get(customer=user)
+            serializer = CartSerializer(cart)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Cart.DoesNotExist:
+            return Response({'error': 'Cart not found'}, status=status.HTTP_404_NOT_FOUND)
 
-    try:
-        cart = Cart.objects.get(customer=user)
-        cart_item = CartItem.objects.get(cart=cart, product_id=product_id)
-        
-        if remove_entire_item or cart_item.quantity <= 1:
-            cart_item.delete()
-        else:
-            cart_item.quantity -= 1
-            cart_item.save()
+    def post(self, request): #edit cart
+        user = request.user
+        product_id = request.data.get('product_id')
+        new_quantity = request.data.get('new_quantity', None)
+        remove_entire_item = request.data.get('remove_entire_item', False)
 
-        return Response({'success': 'Item updated in cart'}, status=status.HTTP_200_OK)
-    except Cart.DoesNotExist:
-        return Response({'error': 'Cart not found'}, status=status.HTTP_404_NOT_FOUND)
-    except CartItem.DoesNotExist:
-        return Response({'error': 'Item not found in cart'}, status=status.HTTP_404_NOT_FOUND)
+        try:
+            cart = Cart.objects.get(customer=user)
+            cart_item = CartItem.objects.get(cart=cart, product_id=product_id)
+            
+            if remove_entire_item:
+                cart_item.delete()
+            elif new_quantity is not None:
+                cart_item.quantity = new_quantity
+                cart_item.save()
+            else:
+                return Response({'error': 'No edit parameters provided'}, status=status.HTTP_400_BAD_REQUEST)
+
+            cart.refresh_from_db()
+            serializer = CartSerializer(cart)
+            return Response({'success': 'Cart updated', 'cart': serializer.data}, status=status.HTTP_200_OK)
+        except Cart.DoesNotExist:
+            return Response({'error': 'Cart not found'}, status=status.HTTP_404_NOT_FOUND)
+        except CartItem.DoesNotExist:
+            return Response({'error': 'Item not found in cart'}, status=status.HTTP_404_NOT_FOUND)
